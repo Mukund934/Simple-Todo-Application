fd204571f9de12cb095a781f5d010855c331fba7
Mukund934 <mukund.th04@gmail.com>
2025-02-10 23:30:17 +0530

    frontend work

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,9 +3,22 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import { CreateTodo } from "./components/CreateTodo";
-import {Todos} = from "./components/Todos"
+
+
+
+
+// import { Todos } = from "./components/Todos"
 function App() {
-	
+	const [todos, setTodos] = useState([]);
+
+	fetch("http://localhost:3000/todos")
+		.then(async function (res) {
+			const json = await res.json();
+			setTodos(json.todos);
+		})
+		.catch(function (error) {
+			console.error("Error fetching todos:", error);
+		});
 
 	return (
 		<div>
