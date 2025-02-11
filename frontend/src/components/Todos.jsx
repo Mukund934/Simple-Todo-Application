b0a962947b4ecef2090129ced6fe75c984c05bd9
Mukund934 <mukund.th04@gmail.com>
2025-02-11 11:38:54 +0530

    fixed issues in Todos.jsx

--- frontend/src/components/Todos.jsx
+++ frontend/src/components/Todos.jsx
@@ -1,19 +1,18 @@
 export function Todos({ todos }) {
 	return (
-		<div>
-			{todos.map(function (todo) {
-				return (
-					<div>
-						<h1>{todos.title}</h1>
-						<h2>{todos.description}</h2>
-						<button>
-							{todos.complete == true
-								? "Completed"
-								: "Mark as complete"}
-						</button>
-					</div>
-				);
-			})}
-		</div>
+	  <div>
+		{todos.map(function (todo) {
+		  return (
+			<div key={todo.id}>
+			  <h1>{todo.title}</h1>
+			  <h2>{todo.description}</h2>
+			  <button>
+				{todo.complete === true ? "Completed" : "Mark as complete"}
+			  </button>
+			</div>
+		  );
+		})}
+	  </div>
 	);
-}
+  }
+  
