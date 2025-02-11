39468c4983362afcd9c584f882274547e7136816
Mukund934 <mukund.th04@gmail.com>
2025-02-11 11:33:56 +0530

    fixed CreatedTodo, added function for fetching

--- frontend/src/components/CreateTodo.jsx
+++ frontend/src/components/CreateTodo.jsx
@@ -1,57 +1,56 @@
-export function CreateTodo(props) {
-	const [title, setTitle] = useState("");
-	const [description, setDescription] = useState("");
-	return (
-		<div>
-			<input
-				style={{
-					padding: 10,
-					margin: 10,
-				}}
-				type="text"
-				placeholder="title"
-				onChange={function (e) {
-					const value = e.target.value;
-					setTitle(e.target.value);
-				}}
-			></input>
-			<br></br>
-			<input
-				style={{
-					padding: 10,
-					margin: 10,
-				}}
-				type="text"
-				placeholder="description"
-				onChange={function (e) {
-					const value = e.target.value;
-					setDescription(e.target.value);
-				}}
-			></input>
+import { useState } from "react";
 
-			<button
-				style={{
-					padding: 10,
-					margin: 10,
-				}}
-				onClick={function () {
-					fetch("http://localhost:3000/todos", {
-						method: "POST",
-						body: JSON.stringify({
-							title: "",
-							description: "jjjjj",
-						}),
-                        headers: {
-                            "contentType": "application/json"
-                        }
-					}).then(async function (res) {
-						const json = await res.json();
-						alert("Todo added");
-					});
-				}}
-			>
-				Add a todo
-			</button>
-		</div>
-	);
+export function CreateTodo(props) {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  function handleAddTodo() {
+    fetch("http://localhost:3000/todos", {
+      method: "POST",
+      body: JSON.stringify({
+        title: title,
+        description: description,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then(async function (res) {
+        const json = await res.json();
+        alert("Todo added");
+      })
+      .catch(function (error) {
+        console.error("Error adding todo:", error);
+      });
+  }
+
+  return (
+    <div>
+      <input
+        style={{ padding: 10, margin: 10 }}
+        type="text"
+        placeholder="title"
+        value={title}
+        onChange={function (e) {
+          setTitle(e.target.value);
+        }}
+      />
+      <br />
+      <input
+        style={{ padding: 10, margin: 10 }}
+        type="text"
+        placeholder="description"
+        value={description}
+        onChange={function (e) {
+          setDescription(e.target.value);
+        }}
+      />
+      <button
+        style={{ padding: 10, margin: 10 }}
+        onClick={handleAddTodo}
+      >
+        Add a todo
+      </button>
+    </div>
+  );
 }
