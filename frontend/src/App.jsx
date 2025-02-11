ae1f780a5825d17875ca2baecaab41987738e58d
Mukund934 <mukund.th04@gmail.com>
2025-02-11 11:35:22 +0530

    useEffect in app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,31 +1,31 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import { CreateTodo } from "./components/CreateTodo";
+import { Todos } from "./components/Todos";
 
-
-
-
-// import { Todos } = from "./components/Todos"
 function App() {
-	const [todos, setTodos] = useState([]);
-	
+  const [todos, setTodos] = useState([]);
 
-	fetch("http://localhost:3000/todos")
-		.then(async function (res) {
-			const json = await res.json();
-			setTodos(json.todos);
-		})
-		.catch(function (error) {
-			console.error("Error fetching todos:", error);
-		});
+  // Use useEffect to fetch todos when the component mounts
+  useEffect(function () {
+    fetch("http://localhost:3000/todos")
+      .then(async function (res) {
+        const json = await res.json();
+        setTodos(json.todos);
+      })
+      .catch(function (error) {
+        console.error("Error fetching todos:", error);
+      });
+  }, []);
 
-	return (
-		<div>
-			<CreateTodo></CreateTodo>
-		</div>
-	);
+  return (
+    <div>
+      <CreateTodo />
+      <Todos todos={todos} />
+    </div>
+  );
 }
 
 export default App;
