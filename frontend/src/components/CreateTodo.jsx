99299ae0e08315348004b91d73f3463836f9154d
Mukund934 <mukund.th04@gmail.com>
2025-02-11 11:21:13 +0530

    working on fetching request on change

--- frontend/src/components/CreateTodo.jsx
+++ frontend/src/components/CreateTodo.jsx
@@ -1,21 +1,57 @@
-export function CreateTodo() {
+export function CreateTodo(props) {
+	const [title, setTitle] = useState("");
+	const [description, setDescription] = useState("");
 	return (
 		<div>
-			<input style={{
-                padding: 10,
-                margin: 10
-            }} type="text" placeholder="title"></input>
+			<input
+				style={{
+					padding: 10,
+					margin: 10,
+				}}
+				type="text"
+				placeholder="title"
+				onChange={function (e) {
+					const value = e.target.value;
+					setTitle(e.target.value);
+				}}
+			></input>
 			<br></br>
-			<input style={{
-                padding: 10,
-                margin: 10
-            }} type="text" placeholder="description"></input>
+			<input
+				style={{
+					padding: 10,
+					margin: 10,
+				}}
+				type="text"
+				placeholder="description"
+				onChange={function (e) {
+					const value = e.target.value;
+					setDescription(e.target.value);
+				}}
+			></input>
 
-            <button style={{
-                padding: 10,
-                margin: 10
-            }} >Add a todo</button>
+			<button
+				style={{
+					padding: 10,
+					margin: 10,
+				}}
+				onClick={function () {
+					fetch("http://localhost:3000/todos", {
+						method: "POST",
+						body: JSON.stringify({
+							title: "",
+							description: "jjjjj",
+						}),
+                        headers: {
+                            "contentType": "application/json"
+                        }
+					}).then(async function (res) {
+						const json = await res.json();
+						alert("Todo added");
+					});
+				}}
+			>
+				Add a todo
+			</button>
 		</div>
-        
 	);
 }
