d69143e14d67bec11cd74e168d39eb84fd8645da
Mukund934 <mukund.th04@gmail.com>
2025-02-12 19:33:06 +0530

    Update todo schema to include userId and add user schema for authentication

--- db.js
+++ db.js
@@ -1,23 +1,24 @@
-// {
-//     Todo{
-//         title: string,
-//         description: string,
-//         completed: boolean
-//     }
-// }
-
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb+srv://admin1:SDZ3E9dKfzt5vYEc@cluster0.rxi0w.mongodb.net/todos");
+mongoose.connect(
+	"mongodb+srv://admin1:SDZ3E9dKfzt5vYEc@cluster0.rxi0w.mongodb.net/todos"
+);
+
 const todoSchema = mongoose.Schema({
 	title: String,
 	description: String,
 	completed: Boolean,
+	userId: String,
 });
-
 const todo = mongoose.model("todos", todoSchema);
 
-module.exports = {
-    todo
-}
+const userSchema = mongoose.Schema({
+	username: { type: String, unique: true },
+	password: String,
+});
+const user = mongoose.model("users", userSchema);
 
+module.exports = {
+	todo: todo,
+	user: user,
+};
