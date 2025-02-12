463b507c4d94a290fda902343e01cd3a86ff2963
Mukund934 <mukund.th04@gmail.com>
2025-02-13 02:06:41 +0530

    update Login

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,53 +1,47 @@
 import { useState } from "react";
 
-function Login(props) {
-	const [username, setUsername] = useState("");
-	const [password, setPassword] = useState("");
+function Login({ setToken }) {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-	function handleLogin(e) {
-		e.preventDefault();
-		fetch("http://localhost:3000/login", {
-			method: "POST",
-			headers: { "Content-Type": "application/json" },
-			body: JSON.stringify({ username: username, password: password }),
-		})
-			.then(function (res) {
-				return res.json();
-			})
-			.then(function (data) {
-				if (data.token) {
-					localStorage.setItem("token", data.token);
-					props.setToken(data.token);
-				} else {
-					alert("Login failed");
-				}
-			})
-			.catch(function (error) {
-				console.error("Login error:", error);
-			});
-	}
+  function handleLogin(e) {
+    e.preventDefault();
+    fetch("http://localhost:3000/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ username, password }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.token) {
+          localStorage.setItem("token", data.token);
+          setToken(data.token);
+        } else {
+          alert("Login failed");
+        }
+      })
+      .catch((error) => {
+        console.error("Login error:", error);
+      });
+  }
 
-	return (
-		<form onSubmit={handleLogin}>
-			<input
-				type="text"
-				placeholder="Username"
-				value={username}
-				onChange={function (e) {
-					setUsername(e.target.value);
-				}}
-			/>
-			<input
-				type="password"
-				placeholder="Password"
-				value={password}
-				onChange={function (e) {
-					setPassword(e.target.value);
-				}}
-			/>
-			<button type="submit">Login</button>
-		</form>
-	);
+  return (
+    <form onSubmit={handleLogin}>
+      <input
+        type="text"
+        placeholder="Username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button type="submit">Login</button>
+    </form>
+  );
 }
 
 export { Login };
