7c144b03144d4be7c50c03b8e5a53df6565b5520
Mukund934 <mukund.th04@gmail.com>
2025-02-13 04:15:02 +0530

    minor improvements

--- backend/types.js
+++ backend/types.js
@@ -1,12 +1,12 @@
 const { z } = require("zod");
 
 const createTodo = z.object({
-  title: z.string().min(1, "Title is required"),
-  description: z.string().min(1, "Description is required"),
+	title: z.string().min(1, "Title is required"),
+	description: z.string().min(1, "Description is required"),
 });
 
 const updateTodo = z.object({
-  id: z.string().min(1, "Todo ID is required"),
+	id: z.string().min(1, "Todo ID is required"),
 });
 
 module.exports = { createTodo, updateTodo };
