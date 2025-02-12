2838aa2e9f315e96e8b70f3f92d2c4051bbd0acc
Mukund934 <mukund.th04@gmail.com>
2025-02-12 19:39:01 +0530

    Update Todos component to add functionality for marking todos as complete

--- frontend/src/components/Todos.jsx
+++ frontend/src/components/Todos.jsx
@@ -1,18 +1,43 @@
-export function Todos({ todos }) {
+function Todos(props) {
+	function markCompleted(id) {
+		fetch("http://localhost:3000/completed", {
+			method: "PUT",
+			headers: {
+				"Content-Type": "application/json",
+				Authorization: "Bearer " + props.token,
+			},
+			body: JSON.stringify({ id: id }),
+		})
+			.then(function (res) {
+				return res.json();
+			})
+			.then(function (data) {
+				alert("Todo marked as completed");
+			})
+			.catch(function (error) {
+				console.error("Error updating todo:", error);
+			});
+	}
+
 	return (
-	  <div>
-		{todos.map(function (todo) {
-		  return (
-			<div key={todo.id}>
-			  <h1>{todo.title}</h1>
-			  <h2>{todo.description}</h2>
-			  <button>
-				{todo.complete === true ? "Completed" : "Mark as complete"}
-			  </button>
-			</div>
-		  );
-		})}
-	  </div>
+		<div>
+			{props.todos.map(function (todo) {
+				return (
+					<div key={todo.id}>
+						<h1>{todo.title}</h1>
+						<h2>{todo.description}</h2>
+						<button
+							onClick={function () {
+								markCompleted(todo.id);
+							}}
+						>
+							{todo.completed ? "Completed" : "Mark as complete"}
+						</button>
+					</div>
+				);
+			})}
+		</div>
 	);
-  }
-  
+}
+
+export { Todos };
