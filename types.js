fd94b5f45a7237a88be4a6e0b18295a82b731599
Mukund934 <mukund.th04@gmail.com>
2025-02-12 19:33:55 +0530

    Define Zod schemas for todo creation and update

--- types.js
+++ types.js
@@ -1,15 +1,5 @@
 const zod = require("zod");
 
-// {
-//     title: string,
-//     description: string,
-
-// }
-
-// {
-//     id: string
-// }
-
 const createTodo = zod.object({
 	title: zod.string(),
 	description: zod.string(),
