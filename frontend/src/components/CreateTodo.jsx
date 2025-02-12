f0b3f58bb5e721381278331f6e5b8aee8ee7a95d
Mukund934 <mukund.th04@gmail.com>
2025-02-12 19:38:01 +0530

    Update CreateTodo component to include token header and use correct endpoint

--- frontend/src/components/CreateTodo.jsx
+++ frontend/src/components/CreateTodo.jsx
@@ -1,56 +1,56 @@
 import { useState } from "react";
 
-export function CreateTodo(props) {
-  const [title, setTitle] = useState("");
-  const [description, setDescription] = useState("");
+function CreateTodo(props) {
+	const [title, setTitle] = useState("");
+	const [description, setDescription] = useState("");
 
-  function handleAddTodo() {
-    fetch("http://localhost:3000/todos", {
-      method: "POST",
-      body: JSON.stringify({
-        title: title,
-        description: description,
-      }),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
-      .then(async function (res) {
-        const json = await res.json();
-        alert("Todo added");
-      })
-      .catch(function (error) {
-        console.error("Error adding todo:", error);
-      });
-  }
+	function handleAddTodo() {
+		fetch("http://localhost:3000/todo", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+				Authorization: "Bearer " + props.token,
+			},
+			body: JSON.stringify({ title: title, description: description }),
+		})
+			.then(function (res) {
+				return res.json();
+			})
+			.then(function (data) {
+				alert("Todo added");
+				// Optionally, refresh todos here
+			})
+			.catch(function (error) {
+				console.error("Error adding todo:", error);
+			});
+	}
 
-  return (
-    <div>
-      <input
-        style={{ padding: 10, margin: 10 }}
-        type="text"
-        placeholder="title"
-        value={title}
-        onChange={function (e) {
-          setTitle(e.target.value);
-        }}
-      />
-      <br />
-      <input
-        style={{ padding: 10, margin: 10 }}
-        type="text"
-        placeholder="description"
-        value={description}
-        onChange={function (e) {
-          setDescription(e.target.value);
-        }}
-      />
-      <button
-        style={{ padding: 10, margin: 10 }}
-        onClick={handleAddTodo}
-      >
-        Add a todo
-      </button>
-    </div>
-  );
+	return (
+		<div>
+			<input
+				style={{ padding: 10, margin: 10 }}
+				type="text"
+				placeholder="title"
+				value={title}
+				onChange={function (e) {
+					setTitle(e.target.value);
+				}}
+			/>
+			<br />
+			<input
+				style={{ padding: 10, margin: 10 }}
+				type="text"
+				placeholder="description"
+				value={description}
+				onChange={function (e) {
+					setDescription(e.target.value);
+				}}
+			/>
+			<button style={{ padding: 10, margin: 10 }} onClick={handleAddTodo}>
+				Add a todo
+			</button>
+		</div>
+	);
 }
+
+export { CreateTodo };
